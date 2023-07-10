bc1eca5d646fa69e262c50a0a64d03544bda439d
Onur Yuksel <yksl.onr@gmail.com>
2023-07-10 14:33:09 +0200

    inverted pendulum dynamics and its constants

---
+++ dynamics/constants.py
@@ -0,0 +1,4 @@
+g = 9.81  # gravity
+length = 1  # length of pole
+m = 1  # ball mass
+b_friction = 1  # friction
---
+++ dynamics/inverted_pendulum.py
@@ -0,0 +1,10 @@
+# Dynamics of the Inverted Pendulum
+#  x ̇ = f (x, u)
+import math
+import numpy as np
+from constants import *
+
+
+def f_dynamics_state_space(x1, x2, u):
+    return (1 / m * math.pow(length, 2)) * m * g * length * \
+           np.sin(x1) + u - b_friction * x2
