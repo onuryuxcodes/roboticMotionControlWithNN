891b014e9798d824b5a67f71012ab1b4d095ba71
Onur Yuksel <yksl.onr@gmail.com>
2023-07-11 20:52:54 +0200

    adding comments on dynamics function

--- dynamics/inverted_pendulum.py
+++ dynamics/inverted_pendulum.py
@@ -6,5 +6,10 @@ from dynamics.constants import *
 
 
 def f_dynamics_state_space(x1, x2, u):
+    """
+    x1: θ, the pendulum’s angle from the downward position
+    x2: θ ̇, derivative of θ, the pendulum’s angular velocity
+    u: control input
+    """
     return (1 / m * math.pow(length, 2)) * m * g * length * \
-           np.sin(x1) + u - b_friction * x2
+           rnp.sin(x1) + u - b_friction * x2
