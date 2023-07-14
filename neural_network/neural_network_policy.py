d80f7f286a89ad793012396d65c4593beb14eb5c
Onur Yuksel <yksl.onr@gmail.com>
2023-07-14 21:39:30 +0200

    separating control network from lyapunov

---
+++ neural_network/neural_network_policy.py
@@ -0,0 +1,13 @@
+import torch
+
+
+class NeuralNetworkPolicy(torch.nn.Module):
+
+    def __init__(self, input_dim, neurons_hidden_layer):
+        super(NeuralNetworkPolicy, self).__init__()
+        self.input_layer = torch.nn.Linear(input_dim, neurons_hidden_layer)
+        self.control_layer = torch.nn.Linear(input_dim, 1, bias=False)
+
+    def forward(self, x):
+        return self.control_layer(x)
+
