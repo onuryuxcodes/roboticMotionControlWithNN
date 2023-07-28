963e71401ad5ab2c152c160e5c6f89d86f91874a
Onur Yuksel <yksl.onr@gmail.com>
2023-07-28 13:56:18 +0200

    plotting constants

--- dynamics/constants.py
+++ dynamics/constants.py
@@ -4,3 +4,13 @@ m = 1  # ball mass
 b_friction = 1  # friction
 m_l_square = m*length*length
 m_g_l = m*g*length
+
+col_loss_each_iter = "loss"
+col_u_each_iter = "avg_u"
+col_policy_each_iter = "avg_policy"
+
+column_list = [
+        col_loss_each_iter,
+        col_policy_each_iter,
+        col_u_each_iter
+    ]
