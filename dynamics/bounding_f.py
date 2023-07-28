d9e764af20373a3ddd6bda32057f8325a08a2666
Onur Yuksel <yksl.onr@gmail.com>
2023-07-28 13:55:58 +0200

    bug fix f

--- dynamics/bounding_f.py
+++ dynamics/bounding_f.py
@@ -3,20 +3,34 @@ from dynamics.constants import *
 import torch
 
 
+#def p_1(t):
+#    return 6 * math.exp(-1 * t) + 0.1
+
 def p_1(t):
-    return 6 * math.exp(-1 * t) + 0.1
+    return (2*math.pi - 0.2)*math.exp(-t) + 0.2
 
 
 def derivative_p_1(t):
-    return -6 * math.exp(-1 * t)
+    return (-2*math.pi + 0.2)*math.exp(-t)
 
+
+#def derivative_p_1(t):
+#    return -6 * math.exp(-1 * t)
 
 def p_2(t):
-    return 10 * math.exp(-0.1 * t) + 0.5
+    return 8*math.exp(-0.5 * t) + 2
 
 
 def derivative_p_2(t):
-    return -1 * math.exp(-0.1 * t)
+    return -4 * math.exp(-0.5 * t)
+
+
+#def p_2(t):
+#    return 10 * math.exp(-0.1 * t) + 0.5
+
+
+#def derivative_p_2(t):
+#    return -1 * math.exp(-0.1 * t)
 
 
 def term_exp_e(e, c):
@@ -36,12 +50,12 @@ def f_e1(e1, e2, t):
                                  derivative_p_1(t) * (term_exp_e(e1, -1) / term_exp_e(e1, 1)))
 
 
-def f_e2(e2, t, u):
-    return f_e2_term_1(e2, t) * ((1 / m_l_square) * (m_g_l * math.sin(term_exp_e(e2, -1) / term_exp_e(e2, 1)) +
-                                                     u - b_friction * (term_exp_e(e2, -1) / term_exp_e(e2, 1))) -
+def f_e2(e1, e2, t, u):
+    return f_e2_term_1(e2, t) * ((1 / m_l_square) * (m_g_l * math.sin(p_1(t)*term_exp_e(e1, -1) / term_exp_e(e1, 1)) +
+                                                     u - b_friction * p_2(t) * (term_exp_e(e2, -1) / term_exp_e(e2, 1))) -
                                  derivative_p_2(t) * term_exp_e(e2, -1) / term_exp_e(e2, 1))
 
 
 def f_of_e(e1, e2, t, u):
-    return torch.tensor([[f_e1(e1, e2, t), f_e2(e2, t, u)]])
+    return torch.tensor([[f_e1(e1, e2, t), f_e2(e1, e2, t, u)]])
 
