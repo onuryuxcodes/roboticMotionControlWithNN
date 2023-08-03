a6234d2331d2fd2f16a0c456cfde19a8d429d709
Onur Yuksel <yksl.onr@gmail.com>
2023-08-03 18:14:59 +0200

    add print_invalid_points_count log

--- training/log_messages.py
+++ training/log_messages.py
@@ -2,3 +2,8 @@
 
 def print_loss(loss, iteration):
     print("Iteration : "+str(iteration)+" Loss : "+str(loss))
+
+
+def print_invalid_points_count(count):
+    print(str(count)+" invalid points found.")
+
