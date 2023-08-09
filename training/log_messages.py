1516036924c555d96e8fc271562059a260601925
Onur Yuksel <yksl.onr@gmail.com>
2023-08-09 16:41:24 +0200

    update falsification log message

--- training/log_messages.py
+++ training/log_messages.py
@@ -4,6 +4,6 @@ def print_loss(loss, iteration):
     print("Iteration : "+str(iteration)+" Loss : "+str(loss))
 
 
-def print_invalid_points_count(count):
-    print(str(count)+" invalid points found.")
+def print_invalid_points_count(n, count):
+    print("Out of "+str(n)+" points "+str(count)+" invalid points found.")
 
